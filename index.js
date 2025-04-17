ceaa3aa581c01b23aa942dc0f00eeeb296d6ccc4
Felps1532 <felipebelomi@gmail.com>
2025-04-16 21:04:02 -0300

    Alterei  21:03

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function adicionarTarefa() {
-    let tarefas = document.getElementById('input-tarefas').value;
+    let tarefa = document.getElementById('input-tarefa').value;
     let dataInicio = document.getElementById('input-dataInicio').value;
     let prazo = document.getElementById('input-prazo').value;
     let prioridade = document.getElementById('select-prioridade').value;
-    let = document.getElementById('input-autor').value;
-    let = document.getElementById('input-descricao').value;
+    let autor = document.getElementById('input-autor').value;
+    let descricao = document.getElementById('input-descricao').value;
 
-    let resultado = "";
+    let resultado = 
+        "Tarefa: " + tarefa + "<br>"
+        "Data de início: " + dataInicio + "<br>"
+        "Prazo: " + prazo + "<br>"
+        "Prioridade: " + prioridade + "<br>"
+        "Autor: " + autor + "<br>"
+        "Descrição: " + descricao + "<br>"
+    ;
 
-    
+    document.getElementById('a-fazer').innerHTML = resultado;
 }
